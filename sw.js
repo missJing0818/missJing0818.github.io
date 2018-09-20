06c0a6dbe4340a1919875fbb26c8e4f0b6e40bb6
zhangjing <978015843@qq.com>
2018-09-20 15:09:36 +0800

    Site updated: 2018-09-20 15:09:36

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/sw-register.js","0fa1f318fe11335c52dde1bd8077c96d"]];
+var precacheConfig = [["/sw-register.js","685fb13c18436c5204d75b58b7023421"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
